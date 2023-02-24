9f2bfe9da69dee6a828c2647cab96403c4a5b37e
deviaunte <126050076+deviaunte@users.noreply.github.com>
2023-02-23 20:54:40 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,10 +4,11 @@ function blink () {
     basic.pause(500)
     pins.digitalWritePin(DigitalPin.P1, 0)
 }
-let strip = neopixel.create(DigitalPin.P0, 1, NeoPixelMode.RGB_RGB)
+let strip = neopixel.create(DigitalPin.P0, 1, NeoPixelMode.RGB)
 blink()
-strip.showRainbow(0, 360)
+strip.setBrightness(10)
 basic.forever(function () {
-    strip.clear()
-    strip.setPixelColor(0, neopixel.rgb(0, 255, 255))
+    basic.pause(500)
+    strip.showColor(neopixel.colors(NeoPixelColors.Purple))
+    blink()
 })

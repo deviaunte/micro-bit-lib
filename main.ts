063d9893d0e8085eb6e27bf765f07b440aed183f
deviaunte <126050076+deviaunte@users.noreply.github.com>
2023-02-26 18:40:21 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,11 +4,23 @@ function blink () {
     basic.pause(500)
     pins.digitalWritePin(DigitalPin.P1, 0)
 }
-let strip = neopixel.create(DigitalPin.P0, 1, NeoPixelMode.RGB)
-blink()
-strip.setBrightness(10)
-basic.forever(function () {
+// Verify RGB pixels work.
+function led_test () {
     basic.pause(500)
-    strip.showColor(neopixel.colors(NeoPixelColors.Purple))
+    strip.setBrightness(10)
+    strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    // track of red
+    basic.showString("R")
     blink()
+    strip.showColor(neopixel.colors(NeoPixelColors.Green))
+    basic.showString("")
+    blink()
+    strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+    blink()
+}
+// Two adafruit Neo pixels in parallel
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P0, 2, NeoPixelMode.RGB)
+basic.forever(function () {
+    led_test()
 })
